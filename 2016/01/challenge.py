71c547efda6b56ac3c6816a27595d0510352cf41
Chris Northwood <chris@pling.org.uk>
2016-12-01 18:49:05 +0000

    Day 1 part 1

---
+++ 2016/01/challenge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+INPUT = "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
+
+
+def parse_input(input):
+    for instruction in input.split(', '):
+        yield instruction[0], int(instruction[1:])
+
+TURN_RESULTS = {
+    'N': {'L': 'W', 'R': 'E'},
+    'S': {'L': 'E', 'R': 'W'},
+    'W': {'L': 'S', 'R': 'N'},
+    'E': {'L': 'N', 'R': 'S'},
+}
+
+x = 0
+y = 0
+d = 'N'
+
+for turn, walk in parse_input(INPUT):
+    d = TURN_RESULTS[d][turn]
+    if d == 'N':
+        y += walk
+    elif d == 'S':
+        y -= walk
+    elif d == 'E':
+        x += walk
+    elif d == 'W':
+        x -= walk
+
+print x + y
