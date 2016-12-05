f60b8ea5aa96fea218c2e7cddbaa13ee146c7ffa
Chris Northwood <chris.northwood@bbc.co.uk>
2016-12-05 10:48:39 +0000

    Day 2

--- 2016/05/challenge.py
+++ 2016/05/challenge.py
@@ -3,13 +3,16 @@ from itertools import count
 
 DOOR_ID = 'uqwqemis'
 
-password = ''
+password = [None] * 8
 
 for i in count():
     hash = md5(DOOR_ID + str(i)).hexdigest()
     if hash[:5] == '00000':
-        password += hash[5]
-    if len(password) == 8:
+        position = int(hash[5], 16)
+        if position < 8 and password[position] is None:
+            print password
+            password[position] = hash[6]
+    if len(filter(lambda l: l is None, password)) == 0:
         break
 
-print "Part 1:", password
+print "Part 2:", ''.join(password)
