7e94bba028973c0185fc2a50b55e897f3b7b9900
Chris Northwood <chris@pling.org.uk>
2016-12-18 17:36:59 +0000

    Add part 1

---
+++ 2016/15/challenge.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+from itertools import count
+import re
+
+INPUT_RE = re.compile(r'Disc #(?P<disc_id>\d) has (?P<positions>\d+) positions; at time=0, it is at position (?P<initial>\d+).')
+
+DISCS = []
+Disc = namedtuple('Disc', 'initial positions')
+
+
+def is_open(disc, t):
+    return (disc.initial + t) % disc.positions == 0
+
+with open('input.txt') as input:
+    for line in input:
+        disc_info = INPUT_RE.match(line).groupdict()
+        DISCS.append(Disc(int(disc_info['initial']), int(disc_info['positions'])))
+
+for t in count():
+    if all(is_open(disc, t + i + 1) for i, disc in enumerate(DISCS)):
+        print "Part One:", t
+        break
