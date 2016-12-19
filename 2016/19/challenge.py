6cd1532ae19174ec3e4d6a1b7cd1aeaa2c482f09
Chris Northwood <chris@pling.org.uk>
2016-12-19 20:32:21 +0000

    Working (but slow) part 2

--- 2016/19/challenge.py
+++ 2016/19/challenge.py
@@ -1,18 +1,33 @@
+from collections import deque
+
 INPUT = 3004953
 
-ELVES = {i: 1 for i in xrange(INPUT)}
 
-while len(ELVES) > 1:
-    print "{} left this round".format(len(ELVES))
-    for i in sorted(ELVES.keys()):
-        if i in ELVES:
-            for j in xrange(1, INPUT):
-                steal_from = (i + j) % INPUT
-                if steal_from in ELVES:
-                    # print "{} stole from {}".format(i, steal_from)
-                    ELVES[i] += ELVES[steal_from]
-                    del ELVES[steal_from]
-                    break
+def left_elves(sorted_elves):
+    return {sorted_elves[(i + 1) % len(sorted_elves)] for i in xrange(0, len(sorted_elves), 2)}
 
 
-print "Part One:", ELVES.keys()[0] + 1
+def opposite_elves(sorted_elves):
+    deleted_elves = set()
+    for i, elf in enumerate(sorted_elves):
+        if elf not in deleted_elves:
+            steal_i = ((i + len(sorted_elves)) / 2) % len(sorted_elves)
+            # print "{} steals from {}".format(elf, sorted_elves[steal_i])
+            deleted_elves.add(sorted_elves[steal_i])
+            sorted_elves = sorted_elves[:steal_i] + sorted_elves[steal_i + 1:]
+    return deleted_elves
+
+
+def last_elf_remaining(strategy, num_elves=INPUT):
+    elves = {i + 1 for i in xrange(num_elves)}
+
+    while len(elves) > 1:
+        print "{} left this round".format(len(elves))
+        elves -= strategy(sorted(elves))
+    return elves.pop()
+
+assert last_elf_remaining(left_elves, 5) == 3
+assert last_elf_remaining(opposite_elves, 5) == 2
+
+print "Part One:", last_elf_remaining(left_elves)
+print "Part Two:", last_elf_remaining(opposite_elves)
