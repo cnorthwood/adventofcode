40c46253c79d9c1772b0c639b350173b3bc942fd
Chris Northwood <chris.northwood@bbc.co.uk>
2016-12-23 11:00:05 +0000

    Add multiplication optimisation for part 2

--- 2016/23/challenge.py
+++ 2016/23/challenge.py
@@ -1,5 +1,5 @@
 REGISTERS = {
-    'a': 7,
+    'a': 12,
     'b': 0,
     'c': 0,
     'd': 0,
@@ -58,6 +58,25 @@ def tgl(x):
             INSTRUCTIONS[t] = (jnz, INSTRUCTIONS[t][1])
     return 1
 
+
+def mult_lookahead(instructions):
+    if [op for op, args in instructions] == [inc, dec, jnz, dec, jnz]:
+        reg_a = instructions[1][1][0]
+        reg_b = instructions[3][1][0]
+        if instructions[2][1][0] == reg_a and instructions[4][1][0] == reg_b:
+            return True
+    return False
+
+
+def mult_opt(*instructions):
+    target = instructions[0][1][0]
+    reg_a = instructions[1][1][0]
+    reg_b = instructions[3][1][0]
+    REGISTERS[target] += REGISTERS[reg_a] * REGISTERS[reg_b]
+    REGISTERS[reg_a] = 0
+    REGISTERS[reg_b] = 0
+    return 4
+
 OPS = {
     'cpy': cpy,
     'inc': inc,
@@ -75,6 +94,8 @@ pc = 0
 
 while pc < len(INSTRUCTIONS):
     op, args = INSTRUCTIONS[pc]
+    if mult_lookahead(INSTRUCTIONS[pc:pc+5]):
+        op, args = mult_opt, INSTRUCTIONS[pc:pc+5]
     pc += op(*args)
 
-print "Part One:", REGISTERS['a']
+print "Part Two:", REGISTERS['a']
