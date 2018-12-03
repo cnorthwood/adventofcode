bfc986b5172406833c608d64ea94c339496d6e6f
Chris Northwood <chris@pling.org.uk>
2018-12-03 11:18:15 +0000

    some optimisation

--- 2018/03/challenge.py
+++ 2018/03/challenge.py
@@ -11,33 +11,33 @@ TEST = """
 
 LINE_RE = re.compile(r'#(?P<id>\d+) @ (?P<x>\d+),(?P<y>\d+): (?P<w>\d+)x(?P<h>\d+)')
 
-Claim = namedtuple('Claim', 'id x y w h')
+Claim = namedtuple('Claim', 'id coverage')
+
+
+def covered(origin_x, origin_y, w, h):
+    for x in range(origin_x, origin_x + w):
+        for y in range(origin_y, origin_y + h):
+            yield x, y
 
 
 def parse_line(line):
     match = LINE_RE.match(line)
     return Claim(int(match.group('id')),
-                 int(match.group('x')),
-                 int(match.group('y')),
-                 int(match.group('w')),
-                 int(match.group('h')))
+                 list(covered(int(match.group('x')),
+                              int(match.group('y')),
+                              int(match.group('w')),
+                              int(match.group('h')))))
 
 
-test_claims = list(map(parse_line, TEST.strip().splitlines()))
+# test_claims = list(map(parse_line, TEST.strip().splitlines()))
 with open('input.txt') as input_file:
     CLAIMS = list(map(parse_line, input_file.read().strip().splitlines()))
-
-
-def covered(claim):
-    for x in range(claim.x, claim.x + claim.w):
-        for y in range(claim.y, claim.y + claim.h):
-            yield x, y
 
 
 def build_cloth_claims(claims):
     squares = Counter()
     for claim in claims:
-        for x, y in covered(claim):
+        for x, y in claim.coverage:
             squares[x, y] += 1
     return squares
 
@@ -46,18 +46,18 @@ def overlapping_squares(squares):
     return len([n for n in squares.values() if n > 1])
 
 
-test_cloth_claims = build_cloth_claims(test_claims)
+# test_cloth_claims = build_cloth_claims(test_claims)
 CLOTH_CLAIMS = build_cloth_claims(CLAIMS)
 
-assert(overlapping_squares(test_cloth_claims) == 4)
+# assert(overlapping_squares(test_cloth_claims) == 4)
 print("Part One: {}".format(overlapping_squares(CLOTH_CLAIMS)))
 
 
 def find_exact_match(claims, cloth_claims):
     for claim in claims:
-        if all(cloth_claims[x, y] == 1 for x, y in covered(claim)):
+        if all(cloth_claims[x, y] == 1 for x, y in claim.coverage):
             return claim.id
 
 
-assert(find_exact_match(test_claims, test_cloth_claims) == 3)
+# assert(find_exact_match(test_claims, test_cloth_claims) == 3)
 print("Part Two: {}".format(find_exact_match(CLAIMS, CLOTH_CLAIMS)))
