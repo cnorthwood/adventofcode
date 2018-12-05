c4851bab6849071d12888439c801b4ce84874e7a
Chris Northwood <chris@pling.org.uk>
2018-12-05 17:29:49 +0000

    Day 5

---
+++ 2018/05/challenge.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env pypy3
+
+from itertools import chain
+import re
+import string
+
+TEST = 'dabAcCaCBAcCcaDA'
+
+pairs = list(
+    map(lambda p: ''.join(p), chain(
+        zip(string.ascii_lowercase, string.ascii_uppercase),
+        zip(string.ascii_uppercase, string.ascii_lowercase)
+    ))
+)
+
+PAIRS_RE = re.compile('({})'.format('|'.join(pairs)))
+
+
+def annihilate(polymer):
+    polymer, subs_made = PAIRS_RE.subn('', polymer)
+    if subs_made == 0:
+        return len(polymer)
+    else:
+        return annihilate(polymer)
+
+
+assert(annihilate(TEST) == 10)
+
+with open('input.txt') as input_file:
+    INPUT = input_file.read().strip()
+
+print("Part One: {}".format(annihilate(INPUT)))
+
+
+def eliminate_problem(polymer, pair):
+    polymer = re.sub('|'.join(pair), '', polymer)
+    return annihilate(polymer)
+
+
+def part_two(polymer):
+    return min(eliminate_problem(polymer, pair) for pair in pairs)
+
+
+assert(part_two(TEST) == 4)
+print("Part Two: {}".format(part_two(INPUT)))
