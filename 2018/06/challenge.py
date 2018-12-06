25db9d9eb2b748ea34b09e67222824bb2021ffa6
Chris Northwood <chris@pling.org.uk>
2018-12-06 22:47:17 +0000

    fix for other cases

--- 2018/06/challenge.py
+++ 2018/06/challenge.py
@@ -31,38 +31,41 @@ def build_grid(coords):
     all_distances = {
         (x, y): {
             coord: manhattan((x, y), coord) for coord in coords
-        } for x, y in product(range(min_x, max_x), range(min_y, max_y))
+        } for x, y in product(range(min_x, max_x + 1), range(min_y, max_y + 1))
     }
     grid = {}
+    infinite_regions = set()
     for coord, distances in all_distances.items():
-        is_infinite = not (min_x < coord[0] < max_x and min_y < coord[1] < max_y)
         sorted_distances = sorted(distances.items(), key=lambda k: k[1])
         closest_coord, distance = sorted_distances[0]
         if distance == sorted_distances[1][1]:
             closest_coord = None
-        grid[coord] = (closest_coord, is_infinite, sum(distances.values()))
-    return grid
+        grid[coord] = (closest_coord, sum(distances.values()))
+        is_infinite = not (min_x < coord[0] < max_x and min_y < coord[1] < max_y)
+        if is_infinite:
+            infinite_regions.add(closest_coord)
+    return grid, infinite_regions
 
 
-TEST_GRID = build_grid(TEST)
-GRID = build_grid(INPUT)
+TEST_GRID, TEST_INFINITE_REGIONS = build_grid(TEST)
+GRID, INFINITE_REGIONS = build_grid(INPUT)
 
 
-def largest_area(grid):
+def largest_area(grid, infinite_regions):
     areas = Counter()
-    for coord, (region, is_infinite, _) in grid.items():
-        if region is not None and not is_infinite:
+    for coord, (region, _) in grid.items():
+        if region is not None and region not in infinite_regions:
             areas[region] += 1
     return areas.most_common(1)[0][1]
 
 
-assert(largest_area(TEST_GRID) == 17)
-print("Part One: {}".format(largest_area(GRID)))
+assert(largest_area(TEST_GRID, TEST_INFINITE_REGIONS) == 17)
+print("Part One: {}".format(largest_area(GRID, INFINITE_REGIONS)))
 
 
 def safe_region(grid, threshold):
     c = 0
-    for coord, (region, _, size) in grid.items():
+    for coord, (region, size) in grid.items():
         if size < threshold:
             c += 1
     return c
