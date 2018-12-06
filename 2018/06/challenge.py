a26e202bb37799e3413a1fa29da2c7ddaed5e231
Chris Northwood <chris@pling.org.uk>
2018-12-06 16:37:31 +0000

    day 6 part 2

--- 2018/06/challenge.py
+++ 2018/06/challenge.py
@@ -35,12 +35,12 @@ def build_grid(coords):
     }
     grid = {}
     for coord, distances in all_distances.items():
+        is_infinite = not (min_x < coord[0] < max_x and min_y < coord[1] < max_y)
         sorted_distances = sorted(distances.items(), key=lambda k: k[1])
         closest_coord, distance = sorted_distances[0]
         if distance == sorted_distances[1][1]:
-            grid[coord] = None
-        else:
-            grid[coord] = closest_coord
+            closest_coord = None
+        grid[coord] = (closest_coord, is_infinite, sum(distances.values()))
     return grid
 
 
@@ -50,11 +50,23 @@ GRID = build_grid(INPUT)
 
 def largest_area(grid):
     areas = Counter()
-    for coord, region in grid.items():
-        if region is not None:
+    for coord, (region, is_infinite, _) in grid.items():
+        if region is not None and not is_infinite:
             areas[region] += 1
     return areas.most_common(1)[0][1]
 
 
 assert(largest_area(TEST_GRID) == 17)
 print("Part One: {}".format(largest_area(GRID)))
+
+
+def safe_region(grid, threshold):
+    c = 0
+    for coord, (region, _, size) in grid.items():
+        if size < threshold:
+            c += 1
+    return c
+
+
+assert(safe_region(TEST_GRID, 32) == 16)
+print("Part Two: {}".format(safe_region(GRID, 10000)))
