b859a3c49f5e322c636c6bfca1ccc8520127f13a
Chris Northwood <chris@pling.org.uk>
2018-12-07 12:40:32 +0000

    day 7 part 2

--- 2018/07/challenge.py
+++ 2018/07/challenge.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env pypy3
 
-import re
 from collections import defaultdict
-from itertools import chain
+from itertools import count
+import re
 
 INPUT_RE = re.compile(r'Step (?P<prereq>.) must be finished before step (?P<step>.) can begin')
 
@@ -13,8 +13,7 @@ def load_input(filename):
         for line in input_file.read().strip().splitlines():
             match = INPUT_RE.match(line)
             steps[match.group('step')].add(match.group('prereq'))
-    for dep in chain(*steps.values()):
-        steps[dep] |= set()
+            steps[match.group('prereq')] |= set()
     return steps
 
 
@@ -34,3 +33,31 @@ INPUT = load_input('input.txt')
 
 assert(''.join(order(TEST)) == 'CABDFE')
 print("Part One: {}".format(''.join(order(INPUT))))
+
+
+def multi_order(deps, num_workers, step_time=lambda s: ord(s) - 4):
+    deps = {step: set(prereqs) for step, prereqs in deps.items()}
+    workers = [None] * num_workers
+    for t in count():
+        for i, worker in enumerate(workers):
+            if worker is not None:
+                working_on, end_time = worker
+                if end_time == t:
+                    del deps[working_on]
+                    for step in deps.keys():
+                        deps[step] -= {working_on}
+                    worker = None
+
+            if worker is None:
+                next_steps = sorted(filter(lambda step: len(deps[step]) == 0 and step not in {w[0] for w in workers if w is not None}, deps.keys()))
+                if next_steps:
+                    workers[i] = (next_steps[0], t + step_time(next_steps[0]))
+                else:
+                    workers[i] = None
+        if not deps:
+            return t
+
+
+assert(multi_order(TEST, 2, step_time=lambda s: ord(s) - 64) == 15)
+print("Part Two: {}".format(multi_order(INPUT, 5)))
+
