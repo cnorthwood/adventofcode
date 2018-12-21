22b4826930c04d83477bbbefdc77eed53bfc4f98
Chris Northwood <chris@pling.org.uk>
2018-12-21 10:25:44 +0000

    in theory more general version of day 20

--- 2018/20/challenge.py
+++ 2018/20/challenge.py
@@ -31,24 +31,31 @@ def load_input(filename):
     with open(filename) as input_file:
         segments, remaining = build_tree(input_file.read().strip())
         assert(remaining == '')
-        return segments
+    grid = defaultdict(lambda: inf)
+    edges = {(0, 0, 0)}
+    build_grid(segments, grid, edges)
+    return grid
 
 
-def build_grid(tree, grid, x=0, y=0, steps=0):
+def build_grid(tree, grid, edges):
     for segment in tree:
         if isinstance(segment, str):
             if segment == 'N':
-                y -= 1
+                edges = {(x, y - 1, steps + 1) for (x, y, steps) in edges}
             elif segment == 'S':
-                y += 1
+                edges = {(x, y + 1, steps + 1) for (x, y, steps) in edges}
             elif segment == 'W':
-                x -= 1
+                edges = {(x - 1, y, steps + 1) for (x, y, steps) in edges}
             elif segment == 'E':
-                x += 1
-            steps += 1
-            grid[x, y] = min(grid[x, y], steps)
+                edges = {(x + 1, y, steps + 1) for (x, y, steps) in edges}
+            for (x, y, steps) in edges:
+                grid[x, y] = min(grid[x, y], steps)
         else:
-            build_grid(segment, grid, x, y, steps)
+            next_edges = set()
+            for branch in segment:
+                next_edges |= build_grid(branch, grid, edges)
+            edges = next_edges
+    return edges
 
 
 def longest_path(grid):
@@ -59,8 +66,7 @@ def number_of_paths_over(grid, lim=1000):
     return len([1 for v in grid.values() if v >= lim])
 
 
-GRID = defaultdict(lambda: inf)
-build_grid(load_input('input.txt'), GRID)
+GRID = load_input('input.txt')
 
 print("Part One: {}".format(longest_path(GRID)))
 print("Part Two: {}".format(number_of_paths_over(GRID)))
