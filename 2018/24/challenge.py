6c65e2cad99f984fef36f2c39a5b24609ac5b05a
Chris Northwood <chris@pling.org.uk>
2018-12-27 17:52:31 +0000

    some refactor, and do not allocate a target if damage can not be done

--- 2018/24/challenge.py
+++ 2018/24/challenge.py
@@ -9,6 +9,9 @@ IMMUNE_RE = re.compile(r'immune to (?P<immunities>[\w, ]+)')
 
 
 class Army:
+    IMMUNE = 'Immune'
+    INFECTION = 'Infection'
+
     def __init__(self, alignment, units, hp, dpoints, dtype, initiative, immunities, weaknesses):
         self.alignment = alignment
         self.units = units
@@ -19,6 +22,22 @@ class Army:
         self.immunities = immunities
         self.weaknesses = weaknesses
 
+    @classmethod
+    def create_from_line(cls, alignment, line):
+        match = INPUT_RE.match(line)
+        weakness_match = WEAK_RE.search(line)
+        immunity_match = IMMUNE_RE.search(line)
+        return cls(
+            alignment,
+            int(match.group('units')),
+            int(match.group('hp')),
+            int(match.group('damage')),
+            match.group('dtype'),
+            int(match.group('initiative')),
+            immunity_match.group('immunities').split(', ') if immunity_match else [],
+            weakness_match.group('weaknesses').split(', ') if weakness_match else [],
+        )
+
     def copy(self):
         return Army(self.alignment,
                     self.units,
@@ -28,6 +47,23 @@ class Army:
                     self.initiative,
                     list(self.immunities),
                     list(self.weaknesses))
+
+    def effective_power(self, defender=None):
+        multiplier = 1
+        if defender and self.dtype in defender.weaknesses:
+            multiplier = 2
+        elif defender and self.dtype in defender.immunities:
+            multiplier = 0
+        return max(0, self.units) * self.dpoints * multiplier
+
+    def attack(self, defender):
+        defender.units -= self.effective_power(defender) // defender.hp
+
+    def select_target(self, defenders):
+        if defenders:
+            target = max(defenders, key=lambda d: (self.effective_power(d), d.effective_power(), d.initiative))
+            if self.effective_power(target) > 0:
+                return target
 
     def __str__(self):
         return "{}: {} units each with {} hit points with an attack that does {} {} damage at initiative {} (weak to {}; immune to {})".format(
@@ -42,52 +78,10 @@ class Army:
         )
 
 
-def build_armies(lines, alignment):
-    for line in lines:
-        match = INPUT_RE.match(line)
-        weakness_match = WEAK_RE.search(line)
-        immunity_match = IMMUNE_RE.search(line)
-        yield Army(
-            alignment,
-            int(match.group('units')),
-            int(match.group('hp')),
-            int(match.group('damage')),
-            match.group('dtype'),
-            int(match.group('initiative')),
-            immunity_match.group('immunities').split(', ') if immunity_match else [],
-            weakness_match.group('weaknesses').split(', ') if weakness_match else [],
-        )
-
-
 def load_input(filename):
     with open(filename) as input_file:
         immune_lines, infection_lines = input_file.read().strip().split('\n\n')
-    return list(build_armies(immune_lines.splitlines()[1:], 'immune')) + list(build_armies(infection_lines.splitlines()[1:], 'infection'))
-
-
-def damage(attacker, defender):
-    effective_power = attacker.units * attacker.dpoints
-    if attacker.dtype in defender.immunities:
-        return 0
-    elif attacker.dtype in defender.weaknesses:
-        return 2 * effective_power
-    else:
-        return effective_power
-
-
-def select_target(attacker, defenders):
-    if defenders:
-        return max(defenders, key=lambda d: (damage(attacker, d), d.units * d.dpoints, d.initiative))
-    else:
-        return None
-
-
-def assign_targets(armies):
-    assignments = {}
-    for army in sorted(armies, key=lambda a: (a.units * a.dpoints, a.initiative), reverse=True):
-        opponent = 'immune' if army.alignment == 'infection' else 'infection'
-        assignments[army] = select_target(army, [a for a in armies if a.alignment == opponent and a not in assignments.values()])
-    return assignments
+    return [Army.create_from_line(Army.IMMUNE, line) for line in immune_lines.splitlines()[1:]] + [Army.create_from_line(Army.INFECTION, line) for line in infection_lines.splitlines()[1:]]
 
 
 class ReindeerDeathError(Exception):
@@ -98,21 +92,27 @@ class Stalemate(Exception):
     pass
 
 
+def assign_targets(armies):
+    assignments = {}
+    for army in sorted(armies, key=lambda a: (a.units * a.dpoints, a.initiative), reverse=True):
+        opponent = Army.IMMUNE if army.alignment == Army.INFECTION else Army.INFECTION
+        assignments[army] = army.select_target([a for a in armies if a.alignment == opponent and a not in assignments.values()])
+    return assignments
+
+
 def fight(armies, boost_immune=None):
     armies = [a.copy() for a in armies]
     for army in armies:
-        if army.alignment == 'immune' and boost_immune:
+        if army.alignment == Army.IMMUNE and boost_immune:
             army.dpoints += boost_immune
     num_units = sum(a.units for a in armies)
-    while sum(1 for a in armies if a.alignment == 'immune') > 0 and sum(1 for a in armies if a.alignment == 'infection') > 0:
+    while any(a.alignment == Army.IMMUNE for a in armies) and any(a.alignment == Army.INFECTION for a in armies):
         for attacker, defender in sorted(assign_targets(armies).items(), key=lambda a: a[0].initiative, reverse=True):
-            if attacker.units > 0 and defender is not None:
-                damage_done = damage(attacker, defender)
-                if damage_done:
-                    defender.units -= damage_done // defender.hp
+            if defender is not None:
+                attacker.attack(defender)
                 if defender.units <= 0:
                     armies.remove(defender)
-        if boost_immune and sum(1 for a in armies if a.alignment == 'immune') == 0:
+        if boost_immune and not any(a.alignment == Army.IMMUNE for a in armies):
             raise ReindeerDeathError()
         next_num_units = sum(a.units for a in armies)
         if num_units == next_num_units:
