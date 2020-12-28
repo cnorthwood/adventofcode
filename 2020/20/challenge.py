945b2481a973aff9851bc54c5e6f441435d2da84
Chris Northwood <chris@pling.org.uk>
2020-12-28 19:51:59 +0000

    2020 day 20 - part 1 now done

--- 2020/20/challenge.py
+++ 2020/20/challenge.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from itertools import chain
+from math import prod
 
 SQUARE_SIZE = 10
 
@@ -24,116 +24,40 @@ def parse_tiles(input_file):
     return tiles
 
 
-def rotate(tile):
-    return frozenset((-y % SQUARE_SIZE, x) for x, y in tile)
+def edges(tile):
+    top = set()
+    bottom = set()
+    left = set()
+    right = set()
+    topr = set()
+    bottomr = set()
+    leftr = set()
+    rightr = set()
+    for xy in range(SQUARE_SIZE):
+        if (xy, 0) in tile:
+            top.add(xy)
+            topr.add(SQUARE_SIZE - 1 - xy)
+        if (xy, SQUARE_SIZE - 1) in tile:
+            bottom.add(xy)
+            bottomr.add(SQUARE_SIZE - 1 - xy)
+        if (0, xy) in tile:
+            left.add(xy)
+            leftr.add(SQUARE_SIZE - 1 - xy)
+        if (SQUARE_SIZE - 1, xy) in tile:
+            right.add(xy)
+            rightr.add(SQUARE_SIZE - 1 - xy)
+    return frozenset((
+        frozenset(top), frozenset(bottom), frozenset(left), frozenset(right),
+        frozenset(topr), frozenset(bottomr), frozenset(leftr), frozenset(rightr),
+    ))
 
 
-def flip_v(tile):
-    return frozenset((x, -y % SQUARE_SIZE) for x, y in tile)
-
-
-def flip_h(tile):
-    return frozenset((-x % SQUARE_SIZE, y) for x, y in tile)
-
-
-def variants(tile):
-    yield frozenset(tile)
-    tile90 = rotate(tile)
-    yield tile90
-    yield flip_v(tile90)
-    yield flip_h(tile90)
-    tile180 = rotate(tile90)
-    yield tile180
-    yield flip_v(tile180)
-    yield flip_h(tile180)
-    tile270 = rotate(tile180)
-    yield tile270
-    yield flip_v(tile270)
-    yield flip_h(tile270)
-
-
-# @cache
-# def left_edge(tile):
-#     return frozenset(y for x, y in tile if x == 0)
-#
-#
-# @cache
-# def right_edge(tile):
-#     return frozenset(y for x, y in tile if x == SQUARE_SIZE - 1)
-#
-#
-# @cache
-# def top_edge(tile):
-#     return frozenset(x for x, y in tile if y == 0)
-#
-#
-# @cache
-# def bottom_edge(tile):
-#     return frozenset(x for x, y in tile if y == SQUARE_SIZE - 1)
-#
-#
-# def is_neighbour_left(tile, neighbour):
-#     return left_edge(tile) == right_edge(neighbour)
-#
-#
-# def is_neighbour_right(tile, neighbour):
-#     return right_edge(tile) == left_edge(neighbour)
-#
-#
-# def is_neighbour_up(tile, neighbour):
-#     return top_edge(tile) == bottom_edge(neighbour)
-#
-#
-# def is_neighbour_down(tile, neighbour):
-#     return bottom_edge(tile) == top_edge(neighbour)
-#
-#
-# def has_neighbour(tile_id, tile, direction_check):
-#     for potential_id, potential_variants in VARIANTS.items():
-#         if tile_id == potential_id:
-#             continue
-#         for potential_variant in potential_variants:
-#             if direction_check(tile, potential_variant):
-#                 return True
-#     return False
-#
-#
-# def find_top_left_corners():
-#     for tile_id in TILES.keys():
-#         if all(not has_neighbour(tile_id, variant, is_neighbour_up) and not has_neighbour(tile_id, variant, is_neighbour_left) for variant in VARIANTS[tile_id]):
-#             yield tile_id
-
-
-def edge_codes(tile):
-    top = 0
-    bottom = 0
-    left = 0
-    right = 0
-    for x in range(SQUARE_SIZE):
-        if (x, 0) in tile:
-            top |= 1 << x
-        if (x, SQUARE_SIZE - 1) in tile:
-            bottom |= 1 << x
-    for y in range(SQUARE_SIZE):
-        if (0, y) in tile:
-            left |= 1 << y
-        if (SQUARE_SIZE - 1, y) in tile:
-            right |= 1 << y
-    return frozenset({top, left, bottom, right})
-
-
-def find_neighbours(tile_edges):
-    neighbours = {}
-    for tile_id, edges in tile_edges.items():
-        neighbours[tile_id] = set()
-        for neighbour_tile, neighbour_edges in tile_edges.items():
-            if tile_id == neighbour_tile:
-                continue
-            if edges & neighbour_edges:
-                neighbours[tile_id].add(neighbour_tile)
-    return neighbours
+def neighbours(tile_id, all_edges):
+    return {other_id for other_id, other_edges in all_edges.items() if tile_id != other_id and len(all_edges[tile_id] & other_edges) > 0}
 
 
 with open("input.txt") as input_file:
     TILES = parse_tiles(input_file)
-TILE_EDGES = {tile_id: frozenset(chain.from_iterable(edge_codes(variant) for variant in variants(tile))) for tile_id, tile in TILES.items()}
+EDGES = {tile_id: edges(tile) for tile_id, tile in TILES.items()}
+NEIGHBOURS = {tile_id: neighbours(tile_id, EDGES) for tile_id in TILES.keys()}
+print(f"Part One: {prod(tile_id for tile_id, neighbours in NEIGHBOURS.items() if len(neighbours) == 2)}")
