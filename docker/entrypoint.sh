1a315138967222ca8ac31d1557eeec0e175843fb
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2023-12-08 16:22:10 +0000

    protect docker running for Shane's new model

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,20 +5,24 @@ set -eo pipefail
 YEAR="${1}"
 DAY="${2}"
 
-cd /code/${YEAR}/`printf %02d $DAY`/
-python3 -m compileall >/dev/null
+if [ -d "/code/${YEAR}/`printf %02d $DAY`/" ]; then
+    cd /code/${YEAR}/`printf %02d $DAY`/
+    python3 -m compileall >/dev/null
 
-if [ -f "part2.py" ] ; then
-    time sh -c './part1.py; ./part2.py'
-elif [ -f "challenge2.py" ] ; then
-    time sh -c './challenge.py; ./challenge2.py'
-elif [ -f "challenge.py" ] ; then
-    sh -c 'time python3 -S ./challenge.py'
-elif [ -f "code.py" ] ; then
-    sh -c 'time ./code.py'
-elif [ -f "challenge.js" ] ; then
-    sh -c 'time ./challenge.js'
+    if [ -f "part2.py" ] ; then
+        time sh -c './part1.py; ./part2.py'
+    elif [ -f "challenge2.py" ] ; then
+        time sh -c './challenge.py; ./challenge2.py'
+    elif [ -f "challenge.py" ] ; then
+        sh -c 'time python3 -S ./challenge.py'
+    elif [ -f "code.py" ] ; then
+        sh -c 'time ./code.py'
+    elif [ -f "challenge.js" ] ; then
+        sh -c 'time ./challenge.js'
+    else
+        echo "not sure how to run this"
+        exit 1
+    fi
 else
-    echo "not sure how to run this"
-    exit 1
+    echo "nothing to run..."
 fi
