d3460936fb280450162295d97fd6a5f2a59c575f
Chris Northwood <chris@pling.org.uk>
2024-12-01 20:41:05 +0000

    Advent of Code 2024 day 1

---
+++ 2024/01/challenge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+with open("input.txt") as input_file:
+    PAIRS = [tuple(map(int, line.split())) for line in input_file]
+
+SORTED_PAIRS = zip(sorted(pair[0] for pair in PAIRS), sorted(pair[1] for pair in PAIRS))
+MULTIPLIERS = Counter(pair[1] for pair in PAIRS)
+
+print(f"Part One: {sum(abs(pair[0] - pair[1]) for pair in SORTED_PAIRS)}")
+print(f"Part Two: {sum(value * MULTIPLIERS[value] for (value, _) in PAIRS)}")
