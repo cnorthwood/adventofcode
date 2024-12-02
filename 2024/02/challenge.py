6d78d0c95eefe74e866d19957ae0370a85f1c6ec
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2024-12-02 10:02:00 +0000

    2024 day 2 part 1

---
+++ 2024/02/challenge.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def all_descending(ns):
+    return all(a > b for a, b in zip(ns, ns[1:]))
+
+def all_ascending(ns):
+    return all(a < b for a, b in zip(ns, ns[1:]))
+
+def no_big_steps(ns):
+    return all(1 <= abs(a-b) <= 3 for a, b in zip(ns, ns[1:]))
+
+def safe(ns):
+    return no_big_steps(ns) and (all_descending(ns) or all_ascending(ns))
+
+
+with open("input.txt") as input_file:
+    REPORTS = [list(map(int, line.split())) for line in input_file]
+
+print(f"Part One: {sum(1 for report in REPORTS if safe(report))}")
