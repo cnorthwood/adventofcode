e169866173d9acc687c007efacad0ff85942917e
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2024-12-02 10:10:19 +0000

    2024 day 2

--- 2024/02/challenge.py
+++ 2024/02/challenge.py
@@ -12,8 +12,12 @@ def no_big_steps(ns):
 def safe(ns):
     return no_big_steps(ns) and (all_descending(ns) or all_ascending(ns))
 
+def dampened_safe(ns):
+    return safe(ns) or any(safe(ns[:i] + ns[i+1:]) for i in range(len(ns)))
+
 
 with open("input.txt") as input_file:
     REPORTS = [list(map(int, line.split())) for line in input_file]
 
 print(f"Part One: {sum(1 for report in REPORTS if safe(report))}")
+print(f"Part Two: {sum(1 for report in REPORTS if dampened_safe(report))}")
