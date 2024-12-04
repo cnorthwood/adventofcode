f58a35b9aac83b50d12f8f86842e8011a4d646bf
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2024-12-04 14:20:29 +0000

    2024 day 4

--- 2024/04/challenge.py
+++ 2024/04/challenge.py
@@ -13,58 +13,75 @@ def safe_check_char(x, y, expected):
 
     return GRID[y][x] == expected
 
-def check_up_left(x, y):
+def xmas_check_up_left(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x - 1, y - 1, "M") \
         and safe_check_char(x - 2, y - 2, "A") \
         and safe_check_char(x - 3, y - 3, "S")
 
-def check_up_right(x, y):
+def xmas_check_up_right(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x + 1, y - 1, "M") \
         and safe_check_char(x + 2, y - 2, "A") \
         and safe_check_char(x + 3, y - 3, "S")
 
-def check_up(x, y):
+def xmas_check_up(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x, y - 1, "M") \
         and safe_check_char(x, y - 2, "A") \
         and safe_check_char(x, y - 3, "S")
 
-def check_left(x, y):
+def xmas_check_left(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x - 1, y, "M") \
         and safe_check_char(x - 2, y, "A") \
         and safe_check_char(x - 3, y, "S")
 
-def check_right(x, y):
+def xmas_check_right(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x + 1, y, "M") \
         and safe_check_char(x + 2, y, "A") \
         and safe_check_char(x + 3, y, "S")
 
-def check_down_left(x, y):
+def xmas_check_down_left(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x - 1, y + 1, "M") \
         and safe_check_char(x - 2, y + 2, "A") \
         and safe_check_char(x - 3, y + 3, "S")
 
-def check_down_right(x, y):
+def xmas_check_down_right(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x + 1, y + 1, "M") \
         and safe_check_char(x + 2, y + 2, "A") \
         and safe_check_char(x + 3, y + 3, "S")
 
-def check_down(x, y):
+def xmas_check_down(x, y):
     return safe_check_char(x, y, "X") \
         and safe_check_char(x, y + 1, "M") \
         and safe_check_char(x, y + 2, "A") \
         and safe_check_char(x, y + 3, "S")
 
-
-def num_hits(x, y):
-    check_funcs = [check_up, check_up_left, check_up_right, check_left, check_right, check_down, check_down_left, check_down_right]
+def num_xmas(x, y):
+    check_funcs = [xmas_check_up, xmas_check_up_left, xmas_check_up_right, xmas_check_left, xmas_check_right, xmas_check_down, xmas_check_down_left, xmas_check_down_right]
     return sum(1 for f in check_funcs if f(x, y))
 
+def mas_check_down(x, y):
+    return (
+            safe_check_char(x - 1, y - 1, "M") and safe_check_char(x + 1, y + 1, "S")
+        ) or (
+            safe_check_char(x - 1, y - 1, "S") and safe_check_char(x + 1, y + 1, "M")
+        )
 
-print(f"Part One: {sum(num_hits(x, y) for x in range(MAX_X + 1) for y in range(MAX_Y + 1))}")
+def mas_check_up(x, y):
+    return (
+            safe_check_char(x - 1, y + 1, "M") and safe_check_char(x + 1, y - 1, "S")
+        ) or (
+            safe_check_char(x - 1, y + 1, "S") and safe_check_char(x + 1, y - 1, "M")
+        )
+
+def num_x_mas(x, y):
+    return 1 if safe_check_char(x, y, "A") and mas_check_down(x, y) and mas_check_up(x, y) else 0
+
+
+print(f"Part One: {sum(num_xmas(x, y) for x in range(MAX_X + 1) for y in range(MAX_Y + 1))}")
+print(f"Part Two: {sum(num_x_mas(x, y) for x in range(MAX_X + 1) for y in range(MAX_Y + 1))}")
