33975e65d1fe08ee8b6a62de13415553e17c3a9b
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2024-12-09 15:20:31 +0000

    2024 day 8

--- 2024/08/challenge.py
+++ 2024/08/challenge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S pypy3 -S
 
 from collections import defaultdict
-from itertools import permutations
+from itertools import permutations, count
 
 
 def load_input(input_filename):
@@ -14,30 +14,42 @@ def load_input(input_filename):
     return antenna_locations
 
 
+ANTENNA_LOCATIONS = load_input("input.txt")
+MAX_X = max(max(loc[0] for loc in locations) for locations in ANTENNA_LOCATIONS.values())
+MAX_Y = max(max(loc[1] for loc in locations) for locations in ANTENNA_LOCATIONS.values())
+
+
+def node_in_bounds(node):
+    return 0 <= node[0] <= MAX_X and 0 <= node[1] <= MAX_Y
+
+
 def find_antinodes(locations):
     for loc_a, loc_b in permutations(locations, r=2):
         dx = loc_a[0] - loc_b[0]
         dy = loc_a[1] - loc_b[1]
         antinode = loc_b[0] - dx, loc_b[1] - dy
+        if not node_in_bounds(antinode):
+            continue
         yield antinode
 
 
-def all_antinodes_for_antenna(antenna_locations):
-    antinode_locations = {}
+def find_harmonics(locations):
+    for loc_a, loc_b in permutations(locations, r=2):
+        for i in count(start=1):
+            dx = loc_a[0] - loc_b[0]
+            dy = loc_a[1] - loc_b[1]
+            antinode = loc_a[0] - i*dx, loc_a[1] - i*dy
+            if not node_in_bounds(antinode):
+                break
+            yield antinode
+
+
+def all_antinodes(antenna_locations, finder):
+    antinode_locations = set()
     for antenna, locations in antenna_locations.items():
-        antinode_locations[antenna] = set(find_antinodes(locations))
+        antinode_locations |= set(finder(locations))
     return antinode_locations
 
 
-def antinodes_in_bounds(antinode_locations, max_x, max_y):
-    all_antinodes = set()
-    for antinodes in antinode_locations.values():
-        all_antinodes |= antinodes
-    return sum(1 for x, y in all_antinodes if 0 <= x <= max_x and 0 <= y <= max_y)
-
-
-ANTENNA_LOCATIONS = load_input("input.txt")
-MAX_X = max(max(loc[0] for loc in locations) for locations in ANTENNA_LOCATIONS.values())
-MAX_Y = max(max(loc[1] for loc in locations) for locations in ANTENNA_LOCATIONS.values())
-
-print(f"Part One: {antinodes_in_bounds(all_antinodes_for_antenna(ANTENNA_LOCATIONS), MAX_X, MAX_Y)}")
+print(f"Part One: {len(all_antinodes(ANTENNA_LOCATIONS, finder=find_antinodes))}")
+print(f"Part Two: {len(all_antinodes(ANTENNA_LOCATIONS, finder=find_harmonics))}")
