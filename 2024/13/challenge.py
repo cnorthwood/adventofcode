1659f4b11f24a6dff5b63fee8b00fee0ce9288ff
Chris Northwood <chris@pling.org.uk>
2024-12-14 18:42:51 +0000

    2024 day 13

--- 2024/13/challenge.py
+++ 2024/13/challenge.py
@@ -10,7 +10,7 @@ def load_input(input_filename):
         for line in input_file:
             yielded = False
             if not line.strip():
-                yield (adx, ady, bdx, bdy, gx, gy)
+                yield adx, ady, bdx, bdy, gx, gy
                 yielded = True
                 continue
             x, y = list(map(int, re.findall(r'\d+', line)))
@@ -24,7 +24,7 @@ def load_input(input_filename):
                 gx = x
                 gy = y
         if not yielded:
-            yield (adx, ady, bdx, bdy, gx, gy)
+            yield adx, ady, bdx, bdy, gx, gy
 
 
 def solve_presses(game):
@@ -45,8 +45,14 @@ def cost(game):
     return pa * A_COST + pb * B_COST
 
 
+def part2_game(game):
+    adx, ady, bdx, bdy, gx, gy = game
+    return adx, ady, bdx, bdy, gx + 10000000000000, gy + 10000000000000
+
+
 assert(solve_presses((94, 34, 22, 67, 8400, 5400)) == (80, 40))
 assert(not is_solvable(solve_presses((26, 66, 67, 21, 12748, 12176))))
 
 GAMES = list(load_input("input.txt"))
 print(f"Part One: {int(sum(map(cost, GAMES)))}")
+print(f"Part Two: {int(sum(map(cost, map(part2_game, GAMES))))}")
