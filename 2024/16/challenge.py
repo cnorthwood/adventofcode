a305bc3b833391ad471c8576528884e1a4011a92
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2024-12-17 15:39:49 +0000

    2024 day 16 part 2

--- 2024/16/challenge.py
+++ 2024/16/challenge.py
@@ -42,10 +42,8 @@ def generate_neighbours(coord, direction, valid_squares):
             yield 1001, (next_step, turn)
 
 
-def dijkstra_lowest_cost(grid):
+def dijkstra(start, grid):
     valid_squares = {coord for coord, c in grid.items() if c != "#"}
-    start = next(coord for coord in valid_squares if grid[coord] == "S")
-    target = next(coord for coord in valid_squares if grid[coord] == "E")
     costs = {(coord, direction): inf for direction in DIRECTIONS for coord in valid_squares}
     costs[start, "E"] = 0
 
@@ -66,8 +64,42 @@ def dijkstra_lowest_cost(grid):
                 costs[neighbour] = next_cost
                 heappush(queue, (next_cost, neighbour))
 
-    return min(cost for (coord, direction), cost in costs.items() if coord == target)
+    return costs
+
+
+def build_predecessors(grid, costs):
+    valid_squares = {coord for coord, c in grid.items() if c != "#"}
+    predecessors = {(coord, direction): set() for direction in DIRECTIONS for coord in valid_squares}
+
+    for (coord, direction), cost in costs.items():
+        for step_cost, neighbour_state in generate_neighbours(coord, direction, valid_squares):
+            if costs[neighbour_state] == cost + step_cost:
+                predecessors[neighbour_state].add((coord, direction))
+
+    return predecessors
+
+
+def backtrack(state, predecessors):
+    path = {state[0]}
+
+    for predecessor in predecessors[state]:
+        path.add(predecessor[0])
+        path.update(backtrack(predecessor, predecessors))
+
+    return path
+
+
+def valid_end_states(target, costs):
+    lowest_cost = min(cost for (coord, direction), cost in costs.items() if coord == target)
+    return lowest_cost, {(coord, direction) for (coord, direction), cost in costs.items() if coord == target and cost == lowest_cost}
 
 
 INPUT = load_input("input.txt")
-print(f"Part One: {dijkstra_lowest_cost(INPUT)}")
+START = next(coord for coord, c in INPUT.items() if c == "S")
+TARGET = next(coord for coord, c in INPUT.items() if c == "E")
+COSTS = dijkstra(START, INPUT)
+LOWEST_COST, VALID_ENDINGS = valid_end_states(TARGET, COSTS)
+PREDECESSORS = build_predecessors(INPUT, COSTS)
+
+print(f"Part One: {LOWEST_COST}")
+print(f"Part Two: {len(set.union(*(backtrack(state, PREDECESSORS) for state in VALID_ENDINGS)))}")
