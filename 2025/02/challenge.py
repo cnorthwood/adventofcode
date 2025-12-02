12722dea9a9f215f1c8001287087e25564bb4475
Chris Northwood <chris@pling.org.uk>
2025-12-02 11:33:50 +0000

    2025 day 2 part 2

--- 2025/02/challenge.py
+++ 2025/02/challenge.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 
-
-with open("input.txt") as input_file:
-    INPUT = [tuple(int(x) for x in part.split("-")) for part in input_file.read().strip().split(",")]
+from functools import cache
 
 
-def invalid_ids(r):
+def load_input(filename):
+    with open(filename) as input_file:
+        return [tuple(int(x) for x in part.split("-")) for part in input_file.read().strip().split(",")]
+
+
+def is_repeating(s, n):
+    return s == s[:len(s) // n] * n
+
+
+
+def invalid_ids_part_one(r):
     for i in range(r[0], r[1] + 1):
         s = str(i)
         if len(s) % 2 != 0:
             continue
-        if s[:len(s)//2] == s[len(s)//2:]:
+        if is_repeating(s, 2):
             yield i
 
 
-print(f"Part One: {sum(sum(invalid_ids(r)) for r in INPUT)}")
+@cache
+def factors(n):
+    r = []
+    for i in range(2, n + 1):
+        if n % i == 0:
+            r.append(i)
+    return r
+
+
+def invalid_ids_part_two(r):
+    for i in range(r[0], r[1] + 1):
+        s = str(i)
+        for factor in factors(len(s)):
+            if is_repeating(s, factor):
+                yield i
+                break
+
+
+# print(sum(sum(invalid_ids_part_two(r)) for r in load_input('test.txt')))
+
+INPUT = load_input("input.txt")
+
+print(f"Part One: {sum(sum(invalid_ids_part_one(r)) for r in INPUT)}")
+print(f"Part Two: {sum(sum(invalid_ids_part_two(r)) for r in INPUT)}")
