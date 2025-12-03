35f9a39ac08ccef747a679ee26f803c0013ce40d
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2025-12-03 11:30:45 +0000

    tweaks for readability

--- 2025/03/challenge.py
+++ 2025/03/challenge.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from itertools import combinations
-
 
 def load_input(filename):
     with open(filename) as input_file:
@@ -10,15 +8,17 @@ def load_input(filename):
 
 def max_joltage(battery_bank, batteries):
     selected = []
-    current_index = -1
+    window_start = 0
+
     while len(selected) < batteries:
-        if batteries - len(selected) == 1:
-            window = battery_bank[current_index + 1:]
-        else:
-            window = battery_bank[current_index + 1:1-batteries + len(selected)]
-        next_highest = max(battery for battery in window)
+        window_end = 1 - batteries + len(selected)
+        window = battery_bank[window_start:None if window_end == 0 else window_end]
+
+        next_highest = max(window)
+
         selected.append(str(next_highest))
-        current_index = window.index(next_highest) + current_index + 1
+        window_start = window_start + window.index(next_highest) + 1
+
     return int("".join(selected))
 
 INPUT = load_input("input.txt")
