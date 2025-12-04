a14587bf21fedc938da1c17bdbfef2ab712c0f56
Chris Northwood <chris.northwood@citizensadvice.org.uk>
2025-12-04 13:14:32 +0000

    2025 day 4 part 2

--- 2025/04/challenge.py
+++ 2025/04/challenge.py
@@ -27,5 +27,22 @@ def num_neighbours(position, locations):
     return sum(1 for neighbour in neighbours(position) if neighbour in locations)
 
 
+def removable_rolls(locations):
+    rolls_to_remove = set()
+    for location in locations:
+        if num_neighbours(location, locations) < 4:
+            rolls_to_remove.add(location)
+    return rolls_to_remove
+
+
+def unremoveable(locations):
+    rolls_to_remove = removable_rolls(locations)
+    while len(rolls_to_remove) > 0:
+        locations -= rolls_to_remove
+        rolls_to_remove = removable_rolls(locations)
+    return locations
+
+
 ROLL_LOCATIONS = load_input("input.txt")
 print(f"Part One: {sum(1 for position in ROLL_LOCATIONS if num_neighbours(position, ROLL_LOCATIONS) < 4)}")
+print(f"Part Two: {len(ROLL_LOCATIONS) - len(unremoveable(ROLL_LOCATIONS))}")
