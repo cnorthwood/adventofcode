7780c43e4984b26105925fdcec1e61e86c78b48e
Chris Northwood <chris@pling.org.uk>
2025-12-07 22:07:39 +0000

    2025 day 7 part 2

--- 2025/07/challenge.py
+++ 2025/07/challenge.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
+from functools import cache
+
 
 def load_input(filename):
     with open(filename) as input_file:
         lines = input_file.readlines()
     start_x = lines[0].index("S")
-    splitter_xs = [{x for x, c in enumerate(line) if c == "^"} for line in lines]
+    splitter_xs = tuple(frozenset({x for x, c in enumerate(line) if c == "^"}) for line in lines)
     return start_x, splitter_xs
 
 
-def simulate(start_x, splitter_xs):
+def part_one(start_x, splitter_xs):
     n_splits = 0
     rays = {start_x}
     for row_splitter_xs in splitter_xs:
@@ -24,5 +26,17 @@ def simulate(start_x, splitter_xs):
         rays = next_rays
     return n_splits
 
+
+@cache
+def part_two(start_x, splitter_xs):
+    if len(splitter_xs) == 0:
+        return 1
+    if start_x in splitter_xs[0]:
+        return part_two(start_x - 1, splitter_xs[1:]) + part_two(start_x + 1, splitter_xs[1:])
+    else:
+        return part_two(start_x, splitter_xs[1:])
+
+
 START_X, SPLITTER_XS = load_input("input.txt")
-print(f"Part One: {simulate(START_X, SPLITTER_XS)}")
+print(f"Part One: {part_one(START_X, SPLITTER_XS)}")
+print(f"Part Two: {part_two(START_X, SPLITTER_XS)}")
